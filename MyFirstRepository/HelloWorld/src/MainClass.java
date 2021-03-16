9fe517b286e70d1332d7c5ac8aa7c2b76aa88d6c
KapichLaptop <KAPTALLlOB@yandex.ru>
2021-03-16 11:22:57 +0300

    facken commit

--- MyFirstRepository/HelloWorld/src/MainClass.java
+++ MyFirstRepository/HelloWorld/src/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        System.out.println("Fucking commit!!!!!!!!!!!s");
+        System.out.println("I did that chaing on gitHub");
+        System.out.println("Fucken commit");
     }
 }
